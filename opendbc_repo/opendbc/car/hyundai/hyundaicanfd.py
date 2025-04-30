1df126b836f83e0692227619ca5988bf8d514bd0
Taylor Hoshino <taylor224@users.noreply.github.com>
2025-04-30 09:06:25 +0900

    Update hyundaicanfd.py

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -53,7 +53,7 @@ def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_angle, 
     common_values["TORQUE_REQUEST"] = 0
     common_values["STEER_REQ"] = 0
     common_values["LKA_AVAILABLE"] = 3 if lat_active else 0
-    common_values["LKAS_ANGLE_CMD"] = -apply_angle
+    common_values["LKAS_ANGLE_CMD"] = apply_angle
     common_values["LKAS_ANGLE_ACTIVE"] = 2 if lat_active else 1
     common_values["LKAS_ANGLE_MAX_TORQUE"] = max_torque if lat_active else 0
     common_values["NEW_SIGNAL_8"] = 10
